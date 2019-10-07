0f5bedbd525b747ea0ca4e16a601b4cd1dc1f5f3
NCHANG1289 <kathychang1205@gmail.com>
2019-10-07 02:21:02 -0400

    Outlier detection - Distance-Based Outlier Detection Clustering

--- src/main/java/Query4.java
+++ src/main/java/Query4.java
@@ -12,7 +12,12 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
+// The code is for Distance-Based Outlier Detection Clustering. If a point with lesss than k points that are within r distance,
+// then report the point as an outlier.
+// The setUp part is used to define the r - radius and k - counts.
+// The mapper split the dataset into n*n blocks, and distinguish the zone or supporting area of points.
+// The reducer then start calculating the distances between points and report outliers in each zone.
+// The code is worked by Nai-tan Chang.
 public class Query4 {
     public static class OutlierMapper extends Mapper<LongWritable, Text, Text, Text> {
 
